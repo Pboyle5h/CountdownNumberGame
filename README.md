712b24063a710db4875bc4fdced284a2ff7499f4
pboyle5h <pauricboylegti@gmail.com>
2017-04-08 10:41:31 +0100

    Added a random number generater along with ther possible list of numbers

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # CountdownNumberGame
 http://stackoverflow.com/questions/42596101/contract-violation-expected-number-scheme
+http://rosettacode.org/wiki/24_game
+http://rosettacode.org/wiki/24_game/Solve
--- Solution.rkt
+++ Solution.rkt
@@ -1,14 +1,18 @@
 #lang racket
 ;list of numbers to use to try get to the expected number 
-(define l(list 1 2 3 4 5 6 2))
-;The expected nimber we are going to try get close to 
-(define Expected 5)
+(define posList(list 1 1 2 2 3 3 4 4 5 5 6 6 7 7 8 8 9 9 10 10 25 50 75 100))
+
+;The expected number we are going to try get close to randomly genertated: 
+(define (rand) (random 101 999))
+(define Expected (rand))
+Expected
 ;list of the different operators that can be used in the game.
 (define ops (list + * - /))
 
-;The plus function that takes the combinations of the list and adds them together 
-(define (plus l) 
-  (+ (car l) (cadr l)))
 
-(map plus(combinations l 2))
+;The plus function that takes the combinations of the list and adds them together 
+(define (plus posList) 
+  (+ (car posList) (cadr posList)))
+(map plus(combinations posList 2))
+
 
