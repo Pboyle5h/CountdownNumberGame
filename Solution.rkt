8cb61b887907e7d1150d96e671769d74c25305a9
pboyle5h <pauricboylegti@gmail.com>
2017-04-10 16:15:42 +0100

    Added the valid RPN function

--- Solution.rkt
+++ Solution.rkt
@@ -9,10 +9,38 @@ Expected
 ;list of the different operators that can be used in the game.
 (define ops (list + * - /))
 
+;This our original list to create reverse polish notation -1 will represent operators
+;and 1 will represent numbers. It doesnt have two -1's and one 1 because for valid
+;reverse polish notaion it needs to start with two nummbers and end with an operator
+(define start-perm (list -1 -1 -1 -1 1 1 1 1))
 
-;The plus function that takes the combinations of the list and adds them together 
-(define (plus posList) 
-  (+ (car posList) (cadr posList)))
-(map plus(combinations posList 2))
+;This line creates all permutations of the original RPN list and it removes the duplicates 
+(define perms (remove-duplicates (permutations start-perm)))
+
+;This function adds the two 1s to the front of the list and the -1 to the end of the list 
+(define (make-rpn l)
+  (append (list 1 1) l (list -1)))
+
+(make-rpn (car  perms))
+; adds 1 1 to start of all permutations and adds -1 to the end of all permutations
+;define new list called perm list which gets passed to the validRpn? function 
+(define permList (map make-rpn perms))
+
+;expression is a list we pass to valid-rpn? 
+(define (valid-rpn? expression [stack 0][valid'()]) ;create a list inside brackets because they need to be the same size [] when called in map
+  ;if expression is a null list call the first if else call the secon
+  (if (null? expression)
+      ;if stack has one value on the list then return the list otherwise return false
+      (if (= stack 1)
+          valid;replace this with a list 
+          #f)
+      ;if the first value in expression is a 1 then call the first line otherwise call the second which starts recurision
+      (if (= (car expression) 1)
+          (valid-rpn? (cdr expression) (+ 1 stack)(append valid( list (car expression)))) ;appends first value in expression the list valid list
+          (valid-rpn? (cdr expression) (- 1 stack)(append valid (list (car expression)))) ;appends first value in expression the list valid list
+          )))
+;defines valid list which will be populated with the valid rpns
+(define validList(map valid-rpn? permList))
+ validList
 
 
